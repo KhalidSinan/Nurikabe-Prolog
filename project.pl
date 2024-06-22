02ce07334fc31dc2b82660a3e7ddcb0a1a4ffe6b
Abdelaziz <abdeez.aushar@yandex.com>
2024-06-23 00:18:47 +0300

    Fix

--- project.pl
+++ project.pl
@@ -139,19 +139,19 @@ iterate_and_add([(I,J)|T], Visited, Acc, Result) :-
 % 3- for each adjacent cell do the following steps:
 %    - check if we have visited it previously using "member" on (Visited).
 %    - if visited => skip the cell.
-%    - if not visited => add the cell to the current result list (Acc) 
+%    - if not visited => add the cell to the current result list (Acc)
 %      and repeat the same algorithm for this cell.
 all_nearby_cells(I,J, Acc, NewVisited, UpdatedAcc) :-
-    nearby_cells(I,J,Cells), 
+    nearby_cells(I,J,Cells),
     remove_empty_lists(Cells,FilteredCells),
 
     iterate_and_add(FilteredCells, [ (I, J) | Acc], Acc, UpdatedAcc),
     % update the visited list (current visited cell + all the visited cells = new visited list)
-    NewVisited = [ (I, J) | Acc]. 
+    NewVisited = [ (I, J) | Acc].
 
 all_nearby_cells(I, J, AllCells) :-
-    % if the passed cell was fixed cell of number 1, 
-    % then the result is the passed cell only. 
+    % if the passed cell was fixed cell of number 1,
+    % then the result is the passed cell only.
     fxd_cell(I,J,1) -> AllCells = [(I,J)] ;
     all_nearby_cells(I, J, [], [(I, J)], AllCells).
 
@@ -188,16 +188,13 @@ one_fixed_cell_in_island:- true.
 
 
 
-<<<<<<< Updated upstream
 
 sum_list_of_value([], 0).
 sum_list_of_value([H|T],Sum):-
     sum_list_of_value(T,Sum1),
     Sum is H+Sum1.
-=======
-# hamza -start
-% island_number_equals_size :
 
+% island_number_equals_size :
 % in this function i will get all the cells which contain number through (findall) and save cells in list then send the list to other function
 
 find_cells_with_numbers() :-
@@ -218,8 +215,6 @@ walk_on_cells_with_number([(X,Y,Value)|Tail]) :-
 count_of_nearby_cells(X,Y,Value) :-
     all_nearby_cells(X,Y,Result), length(Result, Length) , Length =:= Value .
 
-# hamza-end
-
 
 
 get_all_fxd_cells(Sum):-
@@ -231,8 +226,13 @@ calculate_number_of_cells_sea(Sum):-
     solve_cell(I,J,blue),
     all_nearby_cells(I,J,List),
     length(List, Sum),!.
-=======
->>>>>>> Stashed changes
 
+solved_cell_count(Count) :-
+    findall(_, solve_cell(_,_,_), Cells),
+    length(Cells, Count).
 
-
+one_sea:-
+    calculate_number_of_cells_sea(Sum1),
+    get_all_fxd_cells(Sum2),
+    solved_cell_count(Sum3),
+    Sum1+Sum2-1 =:= Sum3.
